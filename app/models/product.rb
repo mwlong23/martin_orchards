c1bce3a3fa85767150ce47d7b32c9a0df91e63b5
mitchell <meechllada@gmail.com>
2017-12-06 10:38:15 -0800

    add validations and associations to models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ApplicationRecord
+  has_many :reviews
+  validates :title, :presence => true
+  validates :description, :presence => true
+  validates :weight, :presence => true
+  validates :box_price, :presence => true
+  validates :weekly_deliver_cost, :presence => true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,7 @@
 class Review < ApplicationRecord
+  belongs_to :user
+  belongs_to :product
+  validates :title, :presence => true
+  validates :body, :presence => true
+  validates :rating, :presence => true
 end
